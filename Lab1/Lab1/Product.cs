b0222d180ad6bad29b494e065070555847f02249
Simionica Bogdana <simionicabogdana@yahoo.com>
2022-10-11 11:50:32 +0300

    Created product and repo

---
+++ Lab1/Lab1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Product
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private double price { get; set; }
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+
+}
---
+++ Lab1/Lab1/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Repository
+    {
+        private Dictionary<Product, int> _products = new Dictionary<Product, int>();
+        
+
+        public int GetQuantiyProduct(Product product)
+        {
+            return this._products[product];
+        }
+
+        public HashSet<Product> GetAll()
+        {
+            HashSet<Product> products = new HashSet<Product>();
+            foreach(Product product in this._products.Keys)
+            {
+                products.Add(product);
+            }
+            return products;
+        }
+
+        public void Add(Product product, int quanity)
+        {
+            if (this._products.ContainsKey(product))
+            {
+                this._products[product] = this._products[product] + quanity;
+
+            }
+            else
+            {
+                this._products.Add(product, quanity);
+            }
+        }
+    }
+}
